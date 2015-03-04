85d35df938683dd7d0bd0342b77aa0603c2c041d
artnerf <franz.artner@firstdata.at>
2015-03-04 16:47:17 +0100

    Init Project

---
+++ app/src/main/java/com/clover/fda/hellfire03/MiqActivity.java
@@ -0,0 +1,17 @@
+package com.clover.fda.hellfire03;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MiqActivity extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_miq);
+	}
+
+}
