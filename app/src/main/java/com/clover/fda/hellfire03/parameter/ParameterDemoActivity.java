ab6c01836bc8535341469dbfb4d07df5d79f3758
veselskyr <robert.veselsky@firstdata.at>
2015-03-13 16:53:26 +0100

    all parameters for bkse.par set

--- app/src/main/java/com/clover/fda/hellfire03/parameter/ParameterDemoActivity.java
+++ app/src/main/java/com/clover/fda/hellfire03/parameter/ParameterDemoActivity.java
@@ -26,6 +26,7 @@ public class ParameterDemoActivity extends Activity {
         Button btnPrefs = (Button) findViewById(R.id.btnPrefs);
         Button btnGetPrefs = (Button) findViewById(R.id.btnGetPreferences);
         Button btnDispNetwork = (Button) findViewById(R.id.btnDisplayNetworkSettings);
+        Button btnBkseIni = (Button) findViewById(R.id.btnCreateParameterFile);
 
         textView = (TextView) findViewById(R.id.txtPrefs);
 
@@ -60,11 +61,12 @@ public class ParameterDemoActivity extends Activity {
         btnPrefs.setOnClickListener(listener);
         btnGetPrefs.setOnClickListener(listener);
         btnDispNetwork.setOnClickListener(listener);
+        btnBkseIni.setOnClickListener(listener);
     }
 
     private void init_bkse_parameter() {
-        SharedPreferences bkse_ini = PreferenceManager.getDefaultSharedPreferences(ParameterDemoActivity.this);
-        SharedPreferences.Editor editor = bkse_ini.edit();
+        SharedPreferences bkse_par = PreferenceManager.getDefaultSharedPreferences(ParameterDemoActivity.this);
+        SharedPreferences.Editor editor = bkse_par.edit();
 
         editor.putString("terminal_id", "54047754");
         editor.putString("language", "de");
@@ -72,24 +74,27 @@ public class ParameterDemoActivity extends Activity {
         // server
         editor.putString("ip_encryption", "NONE"); // NONE, SSL, DATAWIRE
         editor.putString("server_ip", "217.86.142.9"); // Hamburg server
-        editor.putInt("server_port", 9013);
+        editor.putInt   ("server_port", 9013);
         editor.putString("server_ip2", "217.86.142.9"); // Hamburg server
-        editor.putInt("server_port2", 9013);
+        editor.putInt   ("server_port2", 9013);
+        editor.putString("server_ip2", "217.86.142.9"); // Hamburg server
+        editor.putString("ping_host", "www.orf.at"); // Hamburg server
 
         // printer
         editor.putInt("delay_print", 0);
         editor.putInt("elv_text", 0);
 
         // PIN-Pad
-        editor.putString("pinpad_ip", "192.168.1.3");
-        editor.putString("pp_type", "APASPP"); // INTERN, APASPP
-        editor.putString("pp_cardreader", "SEPERATE"); // SEPERATE, HYBRID
-        editor.putInt   ("pp_port", 9050);
+        editor.putString ("pp_connection", "USB");
+        editor.putString ("pinpad_ip", "192.168.1.3");
+        editor.putString ("pp_type", "APASPP"); // INTERN, APASPP
+        editor.putString ("pp_cardreader", "SEPERATE"); // SEPERATE, HYBRID
+        editor.putInt    ("pp_port", 9050);
 
         // Proxy
         editor.putBoolean("proxy_server", false);
-        editor.putInt    ("proxy_port", 8080);
         editor.putString ("proxy_addr", "");
+        editor.putInt    ("proxy_port", 8080);
         editor.putString ("proxy_user", "");
         editor.putString ("proxy_pwd", "");
 
@@ -103,24 +108,49 @@ public class ParameterDemoActivity extends Activity {
 
 
         // Pay@table
-        editor.putString("paytable_ip", "");
-        editor.putInt("paytable_port", 1234);
+        editor.putString ("paytable_ip", "");
+        editor.putInt    ("paytable_port", 1234);
 
 
         // tracer
-        editor.putString("tracer_ip", "192.168.1.20");
-        editor.putInt   ("tracer_level", 1);
+        editor.putString ("tracer_ip", "192.168.1.20");
+        editor.putInt    ("tracer_level", 1);
 
-        //
-        editor.putString("company_no", "00000000");
-        editor.putString("store_no", "00000000");
-        editor.putString("pos_no", "00000000");
-        editor.putString("currency", "0978");
-        editor.putInt   ("currency_exp", 2);
+        // bkse.dat
+        editor.putString ("company_no", "00000000");
+        editor.putString ("store_no", "00000000");
+        editor.putString ("pos_no", "00000000");
+        editor.putString ("currency", "0978");
+        editor.putInt    ("currency_exp", 2);
+        editor.putString ("ecr_version", "90000001");
+        editor.putInt    ("printer_width", 32);
+        editor.putString ("trace_num", "000000");
+        editor.putString ("receipt_num", "0000");
+        editor.putString ("request_id", "0000");
+        editor.putInt    ("offline_sync", 0);
+        editor.putString ("ta_date", "00000000");
+        editor.putString ("ta_time", "000000");
+        editor.putString ("t_snr", "00000000");
+        editor.putString ("pp_vendor", "FD100");
+        editor.putString ("timer_version", "00000000");
+        editor.putString ("capabilities", "00E00000");
+        editor.putString ("merchant_pwd", "1234");
+        editor.putString ("cashier_pwd", "1234");
+        editor.putString ("service_pwd", "210888");
+        editor.putString ("local_version", "00000000");
+        editor.putString ("last_error", "00000000");
+        editor.putString ("last_error_tag", "00000000");
+        editor.putString ("last_error_text", "1234");
+        editor.putInt    ("signature_len", 192);
+        editor.putBoolean("readcard_beep", false);
+        editor.putBoolean("param_changed", true);
+        editor.putInt    ("split_size", 0);
 
 
         // Commit the edits!
         editor.commit();
+
+        displaySharedPreferences();
     }
 
     private void displayNetworkSettings() {
@@ -130,19 +160,13 @@ public class ParameterDemoActivity extends Activity {
     }
 
     private void displaySharedPreferences() {
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(ParameterDemoActivity.this);
-
-        String username = prefs.getString("username", "Default NickName");
-        String passw = prefs.getString("password", "Default Password");
-        boolean checkBox = prefs.getBoolean("checkBox", false);
-        String listPrefs = prefs.getString("listpref", "Default list prefs");
-        int x = prefs.getInt("xxx", 0);
+        SharedPreferences bkse_par = PreferenceManager.getDefaultSharedPreferences(ParameterDemoActivity.this);
 
         StringBuilder builder = new StringBuilder();
-        builder.append("Username: " + username + "\n");
-        builder.append("Password: " + passw + "\n");
-        builder.append("Keep me logged in: " + String.valueOf(checkBox) + "\n");
-        builder.append("List preference: " + listPrefs);
+        builder.append("terminal_id: " + bkse_par.getString("terminal_id", "") + "\n");
+        builder.append("language: " + bkse_par.getString("language", "") + "\n");
+        builder.append("server_ip: " + bkse_par.getString("server_ip", "") + "\n");
+        builder.append("server_port: " + bkse_par.getInt("server_port", 0) + "\n");
 
         textView.setText(builder.toString());
     }
