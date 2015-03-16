3cfedbc78ebad0b84ae7c3fd63173269526d278e
artnerf <franz.artner@firstdata.at>
2015-03-16 17:09:32 +0100

    DefaultPassword

---
+++ app/src/main/java/com/clover/fda/hellfire03/common/DefaultPassword.java
@@ -0,0 +1,10 @@
+package com.clover.fda.hellfire03.common;
+
+/**
+ * Created by firstdata on 16.03.15.
+ */
+public class DefaultPassword {
+    public static final String MERCHANT = "1234";
+    public static final String CASHIER = "1234";
+    public static final String SERVICE = "210888";
+}
