3daa58c88a5b8bb41b600003d001ea791af72d40
artnerf <franz.artner@firstdata.at>
2015-03-16 16:59:21 +0100

    Param update

--- app/src/main/java/com/clover/fda/hellfire03/parameter/Param.java
+++ app/src/main/java/com/clover/fda/hellfire03/parameter/Param.java
@@ -4,5 +4,129 @@ package com.clover.fda.hellfire03.parameter;
  * Created by firstdata on 13.03.15.
  */
 public class Param {
-    public static final String PARAM_REQUEST_ID ="requestid";
+    public static final String IND_REQUEST_ID ="requestid";
+    public static final String IND_TERMINAL_ID = "terminal_id";
+
+    /*
+
+            #define PARAM_IND_CONNECTIONTYPE        1
+            #define PARAM_IND_OUTSIDELINE           2
+            #define PARAM_IND_AREACODE              3
+            #define PARAM_IND_DIALNUMBER_ANALOG     4
+            */
+    public static final String IND_LANGUAGE = "language";
+            /*
+            #define PARAM_IND_LOCALIP               6
+            #define PARAM_IND_SUBNETMASK            7
+            #define PARAM_IND_GATEWAYIP             8
+            #define PARAM_IND_SERVERIP              9
+            #define PARAM_IND_SERVERPORT           10
+            #define PARAM_IND_STX_ETX_ON           11
+            #define PARAM_IND_KEEP_CONNECTION      12
+            #define PARAM_IND_PINPADIP                  13
+            #define PARAM_IND_X25NUMBER            14
+            #define PARAM_IND_ELV_TEXT_NEEDED      15
+            #define PARAM_IND_TRACERIP             16
+            #define PARAM_IND_PINPAD_TYPE          17
+            #define PARAM_IND_PINPAD_CARDREADER    18
+            #define PARAM_IND_PINPAD_PORT          19
+            #define PARAM_IND_DHCP                 20
+            #define PARAM_IND_TRACER_CONNECTION    21
+            #define PARAM_IND_TRACE_LEVEL          22
+            #define PARAM_IND_DIALNUMBER_ISDN      23
+            #define PARAM_IND_DIAL_METHOD          24
+            #define PARAM_IND_TONE_DETECT          25
+            #define PARAM_IND_AREACODE_DLL         26
+            #define PARAM_IND_DIALNBR_ANALOG_DLL   27
+            #define PARAM_IND_DIALNBR_ISDN_DLL     28
+            #define PARAM_IND_X25NUMBER_DLL        29
+            #define PARAM_IND_SERVERIP_DLL         30
+            #define PARAM_IND_SERVERPORT_DLL       31
+            #define PARAM_IND_DELAY_PRINT          32
+            #define PARAM_IND_MSN                  33
+            #define PARAM_IND_AREACODE2            34
+            #define PARAM_IND_DIALNUMBER_ANALOG2   35
+            #define PARAM_IND_DIALNUMBER_ISDN2     36
+            #define PARAM_IND_X25NUMBER2           37
+            #define PARAM_IND_SERVERIP_2           38
+            #define PARAM_IND_SERVERPORT_2         39
+            #define PARAM_IND_DNS_IP                       40
+            #define PARAM_IND_ECR_CONNECTION       41
+            #define PARAM_IND_ECR_PROTOCOL         42
+            #define PARAM_IND_ECR_PORT             43
+            #define PARAM_IND_ECR_BAUDRATE         44
+            #define PARAM_IND_ECR_LEN_INFO         45
+            #define PARAM_IND_ECR_ENC              46
+            #define PARAM_IND_WIFI_SECURITY        47
+            #define PARAM_IND_PROXY_SERVER         48
+            #define PARAM_IND_PROXY_PORT           49
+            #define PARAM_IND_DNS_IP2              50
+            #define PARAM_IND_IP_ENCRYPTION        51
+            #define PARAM_IND_PINPAD_CONNECTION    52
+            #define PARAM_IND_LOCALIP_WIFI         53
+            #define PARAM_IND_SUBNETMASK_WIFI      54
+            #define PARAM_IND_GATEWAYIP_WIFI       55
+            #define PARAM_IND_DNS_IP_WIFI         56
+            #define PARAM_IND_DNS_IP2_WIFI         57
+            #define PARAM_IND_DHCP_WIFI            58
+            #define PARAM_IND_DHCP_WAIT            59
+            #define PARAM_IND_ECR_COM_PARAM        60
+            #define PARAM_IND_PAY_AT_TABLE_IP      61
+            #define PARAM_IND_PAY_AT_TABLE_PORT    62
+//
+// BkseData indexes
+            #define PARAM_IND_COMPANYNO            64
+            #define PARAM_IND_STORENO              65
+            #define PARAM_IND_POSNO                66
+            */
+    public static final String IND_CURRENCY = "currency";
+    public static final String IND_CURRENCY_EXP = "currency_exp";
+            /*
+            #define PARAM_IND_ECR_VERSION          69
+            #define PARAM_IND_SETUP_VERSION        70
+            #define PARAM_IND_PRINTER_WIDTH        71
+            #define PARAM_IND_TRACE_NUM            72
+            #define PARAM_IND_RECEIPT_NUM          73
+            #define PARAM_IND_REQUEST_ID           74
+            #define PARAM_IND_OFFLINE_SYNC         75
+            #define PARAM_IND_TA_DATE              76
+            #define PARAM_IND_TA_TIME              77
+            #define PARAM_IND_T_SNR                78
+            #define PARAM_IND_PP_VENDOR            79
+            #define PARAM_IND_TIMER_VERSION        80
+            #define PARAM_IND_CAPABILITIES         81
+            #define PARAM_IND_MERCHANT_PWD         82
+            #define PARAM_IND_CASHIER_PWD          83
+            #define PARAM_IND_SERVICE_PWD          84
+            #define PARAM_IND_LOCAL_VERSION        85
+            #define PARAM_IND_LAST_ERROR           86
+            #define PARAM_IND_LAST_ERROR_TAG       87
+            #define PARAM_IND_LAST_ERROR_TEXT      88
+            #define PARAM_IND_MODEM_TYPE          89
+            #define PARAM_IND_SIGNATURE_LEN      90
+            #define PARAM_IND_RC_BEEP             91
+            #define PARAM_IND_PARAM_CHANGED      92
+            #define PARAM_IND_SPLIT_SIZE          93
+            #define PARAM_IND_ECR_RECEIPT          94
+
+            #define PARAM_IND_PROXY_ADDR          128
+            #define PARAM_IND_PROXY_USER          129
+            #define PARAM_IND_PROXY_PWD           130
+            #define PARAM_IND_GPRS_APN           131
+            #define PARAM_IND_GPRS_USER           132
+            #define PARAM_IND_GPRS_PWD           133
+            #define PARAM_IND_WIFI_SSID           134
+            #define PARAM_IND_WIFI_PWD            135
+            #define PARAM_IND_PING_HOST         136
+            #define PARAM_IND_STR_MODEM           137
+
+            #define PARAM_IND_ENDOFBKSEPAR        144
+
+            #define LANGUAGE_GERMAN                 0
+            #define LANGUAGE_ENGLISH                1
+
+            #define COMBO_INPUT_LEN                16
+
+            #define BKSE_PAR_FILE_SIZE           6144
+            */
 }
